3cb8b410e61ed12bc7bd0b2af2699a54f7ae9d4b
paritosh <paritoshbiswas0401@gmail.com>
2025-03-11 17:50:24 +0530

    Implemented MVC pattern

--- app.js
+++ app.js
@@ -8,20 +8,18 @@ const app = express();
 app.set('view engine', 'ejs'); // setting view engine to ejs
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const rootDir = require('./util/path');
+const errorController = require('./controllers/error');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page Not Found'});
-});
+app.use(errorController.get404);
 
 app.listen(3000, (err) => {
     if(err){
--- routes/shop.js
+++ routes/shop.js
@@ -1,20 +1,9 @@
-const path = require('path');
-
 const express = require('express');
 
-const rootDir = require('../util/path');
-const adminData = require('./admin');
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    const products = adminData.products;
-    console.log("Products: ", products);
-    res.render('shop', { 
-        pageTitle: 'Shop',
-        prods: products,
-        path: '/'
-    });
-});
+router.get('/', productsController.getProducts);
 
 module.exports = router;
